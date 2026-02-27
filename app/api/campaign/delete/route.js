c53d8bdb1be218e1c03ef93605e2ff679cc6d726
suftnetrepo <pr@suftnet.com>
2026-02-27 08:40:14 +0000

    Fix issues from git comments

--- app/api/campaign/delete/route.js
+++ app/api/campaign/delete/route.js
@@ -1,7 +1,7 @@
 import { deleteCampaign } from '../../../services/campaignService';
 import { logger } from '../../../../utils/logger';
 import { NextResponse } from 'next/server';
-import { getUserSession } from '@/utils/generateToken';
+import { getUserSession } from '../../../../utils/generateToken';
 
 export const DELETE = async (req) => {
   try {
