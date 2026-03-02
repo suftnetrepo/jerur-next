c0f8bb7275deed60bd961aa328cc04fbac0e4edc
suftnetrepo <pr@suftnet.com>
2026-03-02 11:51:12 +0000

    Fix issues related to mongodb path

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 
 export const mongoConnect = async () => {
-  console.log("MONGO_URI exists:", !!process.env.NEXT_PUBLIC_MONGODB_URL);
-  const connectionUrl = 'mongodb+srv://sr72:Kcmkcm12345!@cluster0.ihqj3.mongodb.net/jerur_next_dev?retryWrites=true&w=majority'
+  const connectionUrl = process.env.NEXT_PUBLIC_MONGODB_URL || 'mongodb+srv://sr72:Kcmkcm12345!@cluster0.ihqj3.mongodb.net/jerur_next_dev?retryWrites=true&w=majority'
 
   if (!connectionUrl) {
     console.error('Error: MONGO_URI is not defined in environment variables.');
