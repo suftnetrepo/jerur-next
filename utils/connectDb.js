608ba82b6202c9983983cfe17bd886deb9ed1b34
suftnetrepo <pr@suftnet.com>
2026-03-02 08:31:55 +0000

    Fix issues with mongodb connections

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const mongoConnect = async () => {
-  const connectionUrl = process.env.NEXT_PUBLIC_MONGODB_URL;
+  const connectionUrl = process.env.NEXT_PUBLIC_MONGODB_URL || 'mongodb+srv://sr72:Kcmkcm12345!@cluster0.ihqj3.mongodb.net/jerur_next_dev?retryWrites=true&w=majority'
 
   if (!connectionUrl) {
     console.error('Error: NEXT_PUBLIC_MONGODB_URL is not defined in environment variables.');
