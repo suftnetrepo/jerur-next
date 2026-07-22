a06e27a2a4de163510877d5fdc48155dca97820b
suftnetrepo <pr@suftnet.com>
2026-07-22 16:35:51 +0100

    fix: add CSS/SCSS type declarations and explicit tsconfig include for global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,16 @@
+declare module '*.css' {
+  const styles: { [className: string]: string };
+  export default styles;
+}
+
+declare module '*.scss' {
+  const styles: { [className: string]: string };
+  export default styles;
+}
+
+declare module 'swiper/css';
+declare module 'swiper/css/free-mode';
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/pagination';
+declare module 'swiper/css/thumbs';
+
